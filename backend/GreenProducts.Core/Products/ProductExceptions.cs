561c2190cc743d1afb98564d2d24079a5b46ac6f
Sebastian Green <sgre@netlight.com>
2025-02-07 13:32:23 +0100

    Explanation for exception-based flow.

--- backend/GreenProducts.Core/Products/ProductExceptions.cs
+++ backend/GreenProducts.Core/Products/ProductExceptions.cs
@@ -1,5 +1,8 @@
 ﻿namespace GreenProducts.Core.Products;
 
+// I opted to do exception based flow for not found and validation errors because of simplicity.
+// Would do result-pattern for more critical applications.
+
 public class ProductNotFoundException(string message) : ApplicationException(message);
 
 public class ProductValidationException(string message) : ApplicationException(message);
